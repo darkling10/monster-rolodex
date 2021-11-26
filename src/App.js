9e9ffc6e6a2ef5a58cda0c99bfab613f178444c2
darkling10 <pathan.abbas10@gmail.com>
2021-11-26 18:59:22 +0530

    Worked some

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from "./logo.svg";
+import "./App.css";
+import { Component } from "react";
+import { CardList } from "./containers/card-list/card-list.components"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(){
+    super();
+
+    this.state={
+      monsters:[]
+    }
+  }
+  
+  componentDidMount(){
+   
+      fetch("https://jsonplaceholder.typicode.com/users")
+      .then(res => res.json())
+      .then(user => this.setState({monsters:user}))
+    
+  }
+  render() {
+    return (
+      <div className="App">
+        <CardList user="Abbas">
+          <h1>Abbas</h1>
+        </CardList>
+        {
+          this.state.monsters.map(monster=>(
+            <h1 key={monster.id}>{ monster.name }</h1>
+          ))
+        }
+      </div>
+    );
+  }
 }
 
 export default App;
